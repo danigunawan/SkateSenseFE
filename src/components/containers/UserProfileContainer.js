1763abe9ff667959385163d4a7e2f7b9f4c66509
Sean Conrad <seanrad@Seans-MacBook-Pro.local>
2018-09-26 18:31:53 -0400

    user profile stuff its broke though

--- src/components/containers/UserProfileContainer.js
+++ src/components/containers/UserProfileContainer.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { getUserData } from '../../action'
+import { connect } from 'react-redux'
 
-const UserProfileContainer = () =>{
-  return(
-    <h1>Profile Page</h1>
-  )
+class UserProfileContainer extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      myBookmarks: '',
+      mySkateSpots: ''
+    }
+  }
+
+  componentDidMount(){
+    this.props.getUserData()
+    .then(data=>{
+      this.setState({myBookmarks:data.bookmarks, mySkateSpots:data.skate_spots})
+    })
+    .then(() => {
+      this.waitForStuffToLoad()
+    })
+  }
+
+  waitForStuffToLoad = () =>{
+    let data = this.state.mySkateSpots[0].name ? this.state.mySkateSpots : null
+    if (data !== null) {
+      return this.state.mySkateSpots[0].name
+    }else {
+      console.log('data is null');
+    }
+  }
+
+  render(){
+    // console.log(this.state.mySkateSpots[0])
+    return(
+      <div>
+        <h1>Profile Page</h1>
+        <center><h2>MySpots</h2></center>
+
+      </div>
+    )
+  }
+
 }
 
-export default UserProfileContainer
+const mapStateToProps = (state) => {
+  return {
+    userData: state.user_data,
+    loadingData: state.loadingData
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+      getUserData: () => dispatch(getUserData()),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserProfileContainer)
