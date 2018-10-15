288975c96a0655b96f75c53d9c5d97b0b8b07a2d
seanrad <seanconrad123@gmail.com>
2018-10-14 23:02:42 -0400

    added code snippet to fix Material UI Typography update

--- src/components/child_components/SkateSpotItem.js
+++ src/components/child_components/SkateSpotItem.js
@@ -9,6 +9,7 @@ import IconButton from '@material-ui/core/IconButton';
 import DirectionsIcon from '@material-ui/icons/Directions'
 import BookMarkButton from './bookmarkButton.js'
 import BottomNavigation from '@material-ui/core/BottomNavigation';
+import { createMuiTheme } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
   typography: {
